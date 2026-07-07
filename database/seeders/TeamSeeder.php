7201b6e6ec6d78dd3251ec54f7817ecac868518b
montherziyad <monther.ziad08@gmail.com>
2026-07-07 12:30:06 +0300

    Rename administration team to management

--- database/seeders/TeamSeeder.php
+++ database/seeders/TeamSeeder.php
@@ -26,7 +26,7 @@ class TeamSeeder extends Seeder
             ['name' => 'Customer Support', 'code' => 'CUSTOMER_SUPPORT', 'description' => 'Support tickets and customer service operations.'],
             ['name' => 'Strategy Team', 'code' => 'STRATEGY', 'description' => 'Campaign strategy, brand narrative, planning, and positioning.'],
             ['name' => 'Operations Management', 'code' => 'OPERATIONS', 'description' => 'Operations leadership and delivery governance.'],
-            ['name' => 'Administration', 'code' => 'ADMINISTRATION', 'description' => 'Executive administration, HR coordination, and company management.'],
+            ['name' => 'Management', 'code' => 'ADMINISTRATION', 'description' => 'Executive management, HR coordination, and company leadership.'],
         ];
 
         foreach ($teams as $team) {
